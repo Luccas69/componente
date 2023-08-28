6e6b4317990eb3efb3c3222497ac0e778508fb59
Luccas69 <luccas.rossarolla@gmail.com>
2023-08-28 19:11:29 -0400

    final

--- src/components/Caixa/index.jsx
+++ src/components/Caixa/index.jsx
@@ -1,13 +1,44 @@
-import styles from './styles.module.css'
+import { useState } from 'react';
+import styles from './styles.module.css';
 
-export default function Caixa (){
-    return(
-        <div className={styles.caixa}>
+export default function Caixa() {
+  const [feedbackEnviado, setFeedbackEnviado] = useState(false);
 
-        <h1 className={styles.titulo}>Esse conteúdo foi útil?</h1>
-        <button className={styles.botao_sim}>😄 Sim</button>
-        <button className={styles.botao_nao}>😢 Não</button>
-        
+  const handleFeedback = (feedback) => {
+    setFeedbackEnviado(true);
+  };
+
+  const handleVoltar = () => {
+    setFeedbackEnviado(false);
+  };
+
+  return (
+    <div className={styles.caixa}>
+      {feedbackEnviado ? (
+        <div>
+          <h1 className={styles.titulo}>Obrigado pelo feedback!</h1>
+          <p>Você pode voltar ao estado anterior clicando no botão abaixo:</p>
+          <button className={styles.botao_voltar} onClick={handleVoltar}>
+            Voltar
+          </button>
         </div>
-    )
+      ) : (
+        <div>
+          <h1 className={styles.titulo}>Esse conteúdo foi útil?</h1>
+          <button
+            className={styles.botao_sim}
+            onClick={() => handleFeedback('sim')}
+          >
+            😄 Sim
+          </button>
+          <button
+            className={styles.botao_nao}
+            onClick={() => handleFeedback('não')}
+          >
+            😢 Não
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
